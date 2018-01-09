269aecef4afcefaa17d14791d0eeb2f035faf79b
Stuart Davies <stuart@interprit.com>
2018-01-09 11:08:48 +1100

    Updated PetApiHttpAdaptorTests with xunit Fluent API

--- AGL.Test.Solution.XUnit.Tests/PetWebApiAdaptorTests.cs
+++ AGL.Test.Solution.XUnit.Tests/PetWebApiAdaptorTests.cs
@@ -7,17 +7,17 @@ namespace AGL.Test.Solution.XUnit.Tests
 {    
     public class PetApiHttpAdaptorTests
     {
-
         [Fact]
         public async void PetWebApiGetPeople_BasicIntegrationTest()
         {
             var mockServiceUrl = "https://raw.githubusercontent.com/stuartjdavies/AGL.Test/master/people.json";
             ////var mockServiceUrl = "http://agl-developer-test.azurewebsites.net/people.json";
 
-            var petOwners = await PetServiceHttpAdaptor.GetPetOwners(mockServiceUrl);
-
-            petOwners.Should().HaveCountGreaterThan(0, "We should receive at least one person");                        
-            Assert.True(petOwners.Any(x => x.Pets.Any()), "We should receive some pets");
+            (await PetServiceHttpAdaptor.GetPetOwners(mockServiceUrl))
+            .Should()
+            .HaveCountGreaterThan(0, "We should receive at least one person")
+            .And
+            .Contain(x => x.Pets.Any(), "We should receive some pets");            
         }
     }   
 }
